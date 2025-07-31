eeb812946df9ad4d6deefc6be25ff0464fb22350
shiivankiit <shivankit7557@gmail.com>
2025-07-31 22:16:56 +0530

    datatypes in js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,16 @@
-"use strict" //treat all code as a newer version
+"use strict" //treat all code as a newer version.
+//number
+//bignint
+//string=>" "
+//boolean=>true/false.
+//nulll=>standalonevalue
+//unsefined
+//symbol used in react a lot. = it is unique
+//object.
+let age=20;
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof undefined);//undefined.
+console.log(typeof null);//object.
+
+
