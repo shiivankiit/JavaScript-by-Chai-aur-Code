d2994d31e3a0d7e9a2c06fef849196ef2103a9f5
shiivankiit <shivankit7557@gmail.com>
2025-08-01 20:51:01 +0530

    Math and numbers in JS

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,49 @@
+const score=400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(typeof balance);
+console.log(balance.toFixed(1));
+const otherNumber=23.8966
+console.log(otherNumber.toPrecision(3)); 
+
+const hundreds =100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++Maths++++++++++++.
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));//round off to the heighst value.
+console.log(Math.floor(4.9));//round off to lowest value.
+console.log(Math.min(4,3,6,7,8));
+console.log(Math.max(8,9,5,3,5));
+
+console.log(Math.random());//always come betwn 0s and 1s.
+console.log((Math.random()*10)+1); 
+console.log(Math.floor(Math.random()*10)+1); 
+
+const min=10;
+const max=20;
+console.log (Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
