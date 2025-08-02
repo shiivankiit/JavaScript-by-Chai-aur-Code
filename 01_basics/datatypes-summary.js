f11e3ae1713ea98b8cb98b6bf124119c1d19046a
shiivankiit <shivankit7557@gmail.com>
2025-08-02 16:46:24 +0530

    More about data-types in Js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,5 +1,5 @@
 //Primitive type
-//7 types :String,Number,Boolean,null,undefined,Symbol,BigInt,
+//7 types :String,Number,Boolean,null,undefined,Symbol,BigInt.
 
 const score=100;
 const scoreValue=100.23;
