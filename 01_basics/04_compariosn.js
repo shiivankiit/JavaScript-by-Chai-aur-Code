e1a0d8d6db5ccb8b91f89ba8b46458cfb97a3d9f
shiivankiit <shivankit7557@gmail.com>
2025-08-14 16:37:38 +0530

    Cooment added

--- 01_basics/04_compariosn.js
+++ 01_basics/04_compariosn.js
@@ -4,14 +4,15 @@ console.log(2<=1);
 
 console.log(null>0);
 console.log(null==0);
-console.log(null>=0);
+console.log(null>=0); //operator cnvrt a null to no 
 
 console.log(undefined == 0);
-console.log(undefined>0);
+console.log(undefined>0);//here undefined cnvtred to Nan.
 console.log(undefined<0);
 
 //=== stric check check datatype and values to
 
+//undefined is not set explicitly while null is set explicitly to the variables.
 
 
 
