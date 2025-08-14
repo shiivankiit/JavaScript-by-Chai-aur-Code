444f1d83dd52f5ba41e0d772d093ca6867a42949
shiivankiit <shivankit7557@gmail.com>
2025-08-14 16:36:43 +0530

    Comment added.

--- 01_basics/01_var.js
+++ 01_basics/01_var.js
@@ -1,5 +1,5 @@
 const accountId = 123456789; //once value is assing we cant change it now.
-let accountEmail = "ankit@gmail.com";
+let accountEmail = "ankit@gmail.com";//block-scoped variable declaration ...re-assigned but not redclared
 var accountpswd= "12345";
 accountcity="Jaipur";
 let accountstate;
