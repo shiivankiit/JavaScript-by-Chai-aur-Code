8aaf2851fb57d3609598df8273bb38391f414931
shiivankiit <shivankit7557@gmail.com>
2025-09-04 22:25:30 +0530

    code improvement with comment

--- 07_Projects/color_changer/script.js
+++ 07_Projects/color_changer/script.js
@@ -6,11 +6,11 @@ const body=document.querySelector('body')
 
 //No we need to iterate through btn using loop..so we use forEach loop bcz it is Node list.
 
-btn.forEach(function(button){
-  console.log(button);
+btn.forEach(function(button){//Here btn is the whole collection of node list..forEach loop iterate through each elem in that collection.
+  console.log(button);//Here we pass button as an argument bcz it's our current element whic handle single btn at a time.
   button.addEventListener('click',function(e){
     console.log(e);
-    console.log(e.target);//you will get attribute and value of the btn.
+    console.log(e.target);//you will get attribute and value of the the event happen.
     
     if(e.target.id==='grey'){
         body.style.backgroundColor=e.target.id
